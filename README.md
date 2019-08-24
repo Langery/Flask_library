a600f26856c326f1ded27e7c295d5f47ec586e5f
Langery <1245066197@qq.com>
2019-08-24 15:56:38 +0800

    rewrite

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
 # Flask_library
+
+### Flask 后台库
+
+> idea
+
+- [ ] 针对多种请求方式需要单独做处理
+- [x] 连接到数据库
+- [ ] 对数据库做数据存储和处理
+- [ ] 待续...
+
+> advice
+- [ ] flask 配置文件
+  - [ ] 数据库链接写成方法
+- [ ] Connection 静态变量
+<<<<<<< HEAD
+- [x] connection 连接池（pymysql 自带连接池）
+=======
+- [X] connection 连接池（pymysql 自带连接池）
+>>>>>>> 17dd59bd26e9da24fd760444730ff31ae87ee7ba
+
+> finished
+
+- [x] 实现前端对后台的链接和通讯
+- [ ] 待续...
+
+### Notes
+
+<<<<<<< HEAD
+ 1. 登录注册都为 POST 请求方式
+
+ ###　MySQL order
+
+ 1. 删除一条数据
+``` sql
+  DELETE FROM tablename WHERE NAME = 'abc'
+=======
+1. 登录注册都为 POST 请求方式
+
+###　MySQL order
+
+1. 删除一条数据
+``` sql
+  DELETE FROM tablename WHERE NAME = 'abc'
+```
+>>>>>>> 17dd59bd26e9da24fd760444730ff31ae87ee7ba
+
+### Links:
+
+1. [Python Flask数据库连接池](https://www.cnblogs.com/supery007/p/8206442.html)
---
+++ server.py
@@ -0,0 +1,77 @@
+import json
+from flask import Flask, render_template, request, redirect, url_for, make_response
+import pymysql
+from DBUtils.PersistentDB import PersistentDB
+
+app = Flask(__name__)
+
+POOL = PersistentDB(
+  creator=pymysql, # use data model
+  maxusage=None, # used more times
+  setsession=[], # the order list before start
+  ping=0, # check MySQL server is available
+  closeable=False, # ignored the `conn.close()` ,
+  threadlocal=None, # save the link object
+  host='106.14.190.190',
+  port=7306,
+  user='',
+  password='',
+  database='',
+  charset='utf8'
+)
+
+# 测试接口
+@app.route('/first', methods=['GET'])
+def first():
+  print('success')
+  name = request.args.get('name')
+  print(request)
+  if name:
+    print(name)
+    res = make_response('Hello Ajax ' + name)
+    return res
+  # return 'First message'
+
+'''
+POST way to save data
+'''
+@app.route('/login', methods=['POST'])
+def login():
+  print('login')
+  conn = POOL.connection()
+
+  username = request.form.get('name')
+  password = request.form.get('password')
+  print(username, password)
+  cursor = conn.cursor()
+  # select
+  sqlName = 'SELECT * FROM login where name = %s'
+  sqlPwd = 'SELECT * FROM login where pwd = %s'
+  # add
+  # find = 'insert into login(name, pwd, nickname) values(%s, %s, %s)'
+  # endFind = cursor.execute(find, [username, password, ''])
+  # print(endFind)
+  rowName = cursor.execute(sqlName,[username])
+  rowPws = cursor.execute(sqlPwd,[password])
+  conn.commit()
+  conn.close() # false closure
+  if rowName >= 1:
+    nameData = True
+  else:
+    nameData = False
+  if rowPws >= 1:
+    pwdData = True
+  else:
+    pwdData = False
+  res = {}
+  res['username'] = nameData
+  res['password'] = pwdData
+  print(res)
+  return json.dumps(res)
+
+@app.route('/register', methods=['POST'])
+def register():
+  print('register')
+  
+if __name__ == '__main__':
+  app.run()
