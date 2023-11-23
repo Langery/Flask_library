86c143015d547dd7be0b2e37237e81b7ef068ce4
Langery <1245066197@qq.com>
2023-11-23 14:31:40 +0800

    add flaskr

---
+++ flaskr/__init__.py
@@ -0,0 +1,24 @@
+# 应用工厂
+
+import os
+
+from flask import Flask
+
+def create_app(test_config=None):
+  app = Flask(__name__, instance_relative_config=True)
+  app.config.from_mapping(
+    SECRET_KEY = 'dev',
+    DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+  )
+
+  if test_config is None:
+    app.config.from_pyfile('config.py', slice=True)
+  else:
+    app.config.from_mapping(test_config)
+
+  try:
+    os.makedirs(app.instance_path)
+  except OSError:
+    pass
+
+  return app
